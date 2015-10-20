0f0f0f0fd8472d828cafbc99ace057a1a2023d2b
Robert Deutz <rdeutz@googlemail.com>
2015-10-20 16:29:32 +0200

    removed insert because I don't need it

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
 var gulp        = require('gulp');
 var replace     = require('gulp-replace');
-var insert      = require('gulp-insert');
 var markdownpdf = require('gulp-markdown-pdf');
 var markdown    = require('gulp-markdown');
-
-//var marked = markdown.marked();
-//var renderer = marked.Renderer();
-//console.log('--------'); 
-//console.log(renderer);
 
 gulp.task('markdown', function () {
     return gulp.src('addphp/babioon-add-php.md')
         .pipe(markdown())
-        .pipe(replace('<hr>', '</div><hr id="system-readmore" /><div class="fulltext">'))
+        .pipe(replace('<hr>', '<hr id="system-readmore" />'))
         .pipe(replace(/<h([1,2,3,4,5,6])(.*?)>/g, '<h$1>'))
         .pipe(replace('<h6>', '<h7>'))
         .pipe(replace('<h5>', '<h6>'))
@@ -26,7 +20,6 @@ gulp.task('markdown', function () {
         .pipe(replace('</h3>', '</h4>'))
         .pipe(replace('</h2>', '</h3>'))
 		.pipe(replace('</h1>', '</h2>'))
-        .pipe(insert.wrap('<div class="introtext">', '</div>'))
         .pipe(gulp.dest('addphp'));
 });
 
